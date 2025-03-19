914df4a5ac00fda70b8e9b2fe0379b15c4aa8bb6
KurlanB <kurlan.bee@gmail.com>
2025-03-19 14:07:50 -0400

    Added Drone fly and battery management with MIA check

--- src/main/java/ca/mcmaster/se2aa4/island/team43/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team43/Drone.java
@@ -1,5 +1,53 @@
 package ca.mcmaster.se2aa4.island.team43;
 
 public class Drone {
-    //Fly,Heading, Scanning, Echoing and finding POIs
+    private Location currentCoordinate;
+    private Orientation currentOrientation;
+    private final EnergyManager energyManager;
+    private final Location startCoordinate;
+
+    public Drone(Location startCoordinate, Orientation startOrientation, int maxBattery){
+        this.startCoordinate = startCoordinate;
+        this.currentCoordinate = startCoordinate;
+        this.currentOrientation = startOrientation;
+        this.energyManager = new EnergyManager(this, maxBattery);
+    }
+
+    public Location getCurrentCoordinate(){
+        return currentCoordinate;
+    }
+
+    public Orientation getCurrentOrientation(){
+        return currentOrientation;
+    }
+
+    public void updateOrientation(Orientation newOrientation){
+        this.currentOrientation = newOrientation;
+    }
+
+    public void fly(){
+        switch(currentOrientation){
+            case NORTH:
+                currentCoordinate = new Location(currentCoordinate.getX(), currentCoordinate.getY() - 1);
+                break;
+            case EAST:
+                currentCoordinate = new Location(currentCoordinate.getX() + 1, currentCoordinate.getY());
+                break;
+            case SOUTH:
+                currentCoordinate = new Location(currentCoordinate.getX(), currentCoordinate.getY() + 1);
+                break;
+            case WEST:
+                currentCoordinate = new Location(currentCoordinate.getX() - 1, currentCoordinate.getY());
+                break;
+        }
+    }
+
+    public void depleteBattery(int cost){
+        energyManager.depleteBattery(cost);
+    }
+
+    public boolean checkBattery(){
+        return energyManager.checkBattery();
+    }
+
 }
